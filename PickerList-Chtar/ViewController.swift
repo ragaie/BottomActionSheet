bb39d834736d9f9dd97fc274010841de6d77fa80
Ragaie alfy <Ragaie@ehabs-bc-macbook-pro.local>
2017-07-19 13:49:18 +0200

    Initial Commit

---
+++ PickerList-Chtar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PickerList-Chtar
+//
+//  Created by Ragaie alfy on 7/19/17.
+//  Copyright © 2017 Ragaie alfy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
